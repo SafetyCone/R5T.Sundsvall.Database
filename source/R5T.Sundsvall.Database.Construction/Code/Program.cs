4951c9f464d9cb4fa90f22ea19e447777e65145f
MinexAutomation <minex.automation@gmail.com>
2019-11-29 21:42:36 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Sundsvall.Database.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Sundsvall.Database.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Sundsvall.Database/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Sundsvall.Database
+{
+    public class Class1
+    {
+    }
+}
