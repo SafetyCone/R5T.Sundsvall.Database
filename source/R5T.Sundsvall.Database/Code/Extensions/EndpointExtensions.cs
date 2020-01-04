42f8bbc131968667c7fce988bca9ea3a0b319774
MinexAutomation <minex.automation@gmail.com>
2020-01-04 11:28:08 -0800

    Improved Endpoint entity-type to app-type conversion to allow null EndpointType values.

--- source/R5T.Sundsvall.Database/Code/Extensions/EndpointExtensions.cs
+++ source/R5T.Sundsvall.Database/Code/Extensions/EndpointExtensions.cs
@@ -11,15 +11,19 @@ namespace R5T.Sundsvall.Database
     {
         public static EndpointInfo ToAppType(this EntityType entity)
         {
-            var endpointInfo = new EndpointInfo()
-            {
-                Identity = entity.GUID,
-                Name = entity.Name,
-                TypeInfo = new EndpointTypeInfo()
+            var endpointTypeInfo = entity.EndpointType is object
+                ? new EndpointTypeInfo()
                 {
                     Identity = entity.EndpointType.GUID,
                     Name = entity.EndpointType.Name,
                 }
+                : null;
+
+            var endpointInfo = new EndpointInfo()
+            {
+                Identity = entity.GUID,
+                Name = entity.Name,
+                TypeInfo = endpointTypeInfo
             };
 
             return endpointInfo;
