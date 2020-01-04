2385209892c92a65b459276f4142ef2151010355
MinexAutomation <minex.automation@gmail.com>
2020-01-04 11:31:46 -0800

    Added extensions for the IEndpointDbContext.

---
+++ source/R5T.Sundsvall.Database/Code/Extensions/IEndpointDbContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+using R5T.Endalia;
+
+using EndpointEntity = R5T.Sundsvall.Database.Entities.Endpoint;
+using EndpointTypeEntity = R5T.Sundsvall.Database.Entities.EndpointType;
+
+
+namespace R5T.Sundsvall.Database
+{
+    public static class IEndpointDbContextExtensions
+    {
+        public static IQueryable<EndpointEntity> GetEndpoint(this IEndpointDbContext dbContext, EndpointIdentity endpointIdentity)
+        {
+            var endpointQueryable = dbContext.Endpoints.Where(x => x.GUID == endpointIdentity.Value);
+            return endpointQueryable;
+        }
+
+        public static IQueryable<EndpointTypeEntity> GetEndpointType(this IEndpointDbContext dbContext, EndpointTypeIdentity endpointTypeIdentity)
+        {
+            var endpointTypeQueryable = dbContext.EndpointTypes.Where(x => x.GUID == endpointTypeIdentity.Value);
+            return endpointTypeQueryable;
+        }
+    }
+}
